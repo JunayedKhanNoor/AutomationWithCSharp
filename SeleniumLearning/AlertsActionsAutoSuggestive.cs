211d9aedf4b00977f4e49b220e394b2e480298df
163002029.junayed <junayednoor@gmail.com>
2022-11-14 22:07:49 +0600

    Switch to Allert

---
+++ SeleniumLearning/AlertsActionsAutoSuggestive.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning
+{
+    public class AlertsActionsAutoSuggestive
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/AutomationPractice/";
+        }
+        [Test]
+        public void test_Alert()
+        {
+            String name = "Junayed";
+            driver.FindElement(By.CssSelector("#name")).SendKeys(name);
+            driver.FindElement(By.CssSelector("input[onClick*='displayConfirm']")).Click();
+            String alert_text = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().Accept();
+            StringAssert.Contains(name, alert_text);
+           
+        }
+    }
+}
