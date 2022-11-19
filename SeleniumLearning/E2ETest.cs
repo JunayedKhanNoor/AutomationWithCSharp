116286213e1e86ec3a538c4c75ca2ed7a05d30ac
163002029.junayed <junayednoor@gmail.com>
2022-11-19 08:28:46 +0600

    End to end test, Add to Cart, Purchase and verify

--- SeleniumLearning/E2ETest.cs
+++ SeleniumLearning/E2ETest.cs
@@ -30,6 +30,7 @@ namespace SeleniumLearning
         public void EndToEndFlow()
         {
             String[] expectedProducts = { "iphone X", "Blackberry" };
+            String[] actualProducts = new String[2];
             driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");
             driver.FindElement(By.Name("password")).SendKeys("learning");
             driver.FindElement(By.XPath("//*[@id=\"terms\"]")).Click();
@@ -49,11 +50,27 @@ namespace SeleniumLearning
                 }
             }
             driver.FindElement(By.PartialLinkText("Checkout")).Click();
-            IWebElement header = driver.FindElement(By.CssSelector("div.container div.row div.col-sm-12.col-md-10.col-md-offset-1 table.table.table-hover tbody:nth-child(2) tr:nth-child(3) td:nth-child(4) > h3:nth-child(1)"));
-            TestContext.Progress.WriteLine(header.Text);
-            String expected = "Total";
-            Assert.That(header.Text, Is.EqualTo(expected));
+            //IWebElement header = driver.FindElement(By.CssSelector("div.container div.row div.col-sm-12.col-md-10.col-md-offset-1 table.table.table-hover tbody:nth-child(2) tr:nth-child(3) td:nth-child(4) > h3:nth-child(1)"));
+            //TestContext.Progress.WriteLine(header.Text);
+            //String expected = "Total";
+            //Assert.That(header.Text, Is.EqualTo(expected));
             //driver.Quit();
+
+            IList<IWebElement> checkoutCards = driver.FindElements(By.CssSelector("h4 a"));
+            for (int i = 0; i < checkoutCards.Count; i++)
+            {
+                actualProducts[i] = checkoutCards[i].Text;
+            }
+            Assert.That(actualProducts, Is.EqualTo(expectedProducts));
+            driver.FindElement(By.CssSelector(".btn-success")).Click();
+            driver.FindElement(By.Id("country")).SendKeys("ind");
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.LinkText("India")));
+            driver.FindElement(By.LinkText("India")).Click();
+            driver.FindElement(By.XPath("//label[@for='checkbox2']")).Click();
+            driver.FindElement(By.CssSelector("input[value='Purchase']")).Click();
+            String confirmText = driver.FindElement(By.CssSelector(".alert-success")).Text;
+            StringAssert.Contains("Success", confirmText);
+            driver.Quit();
         }
     }
 }
